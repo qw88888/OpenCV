6c6dd2f28b86d6b22ead633d656bb565b6ad7e6a
qw <88542243+qw88888@users.noreply.github.com>
2022-04-06 15:48:26 +0800

    Add files via upload

---
+++ opencv/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/DependInfo.cmake
@@ -0,0 +1,74 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jaricom.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jaricom.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcapimin.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcapimin.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcapistd.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcapistd.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcarith.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcarith.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jccoefct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jccoefct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jccolor.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jccolor.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcdctmgr.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcdctmgr.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jchuff.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jchuff.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcicc.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcicc.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcinit.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcinit.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcmainct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcmainct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcmarker.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcmarker.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcmaster.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcmaster.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcomapi.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcomapi.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcparam.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcparam.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcphuff.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcphuff.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcprepct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcprepct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jcsample.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jcsample.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jctrans.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jctrans.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdapimin.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdapimin.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdapistd.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdapistd.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdarith.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdarith.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdatadst.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdatadst.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdatasrc.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdatasrc.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdcoefct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdcoefct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdcolor.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdcolor.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jddctmgr.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jddctmgr.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdhuff.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdhuff.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdicc.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdicc.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdinput.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdinput.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdmainct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdmainct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdmarker.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdmarker.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdmaster.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdmaster.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdmerge.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdmerge.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdphuff.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdphuff.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdpostct.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdpostct.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdsample.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdsample.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jdtrans.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jdtrans.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jerror.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jerror.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jfdctflt.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jfdctflt.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jfdctfst.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jfdctfst.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jfdctint.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jfdctint.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jidctflt.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jidctflt.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jidctfst.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jidctfst.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jidctint.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jidctint.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jidctred.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jidctred.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jmemmgr.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jmemmgr.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jmemnobs.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jmemnobs.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jquant1.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jquant1.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jquant2.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jquant2.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jsimd_none.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jsimd_none.c.o"
+  "/home/qw/opencv-3.4.15/3rdparty/libjpeg-turbo/src/jutils.c" "/home/qw/opencv-3.4.15/build/3rdparty/libjpeg-turbo/CMakeFiles/libjpeg-turbo.dir/src/jutils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "3rdparty/libjpeg-turbo"
+  "../3rdparty/libjpeg-turbo/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ opencv/build/3rdparty/libpng/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/qw/opencv-3.4.15/3rdparty/libpng
+# Build directory: /home/qw/opencv-3.4.15/build/3rdparty/libpng
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
