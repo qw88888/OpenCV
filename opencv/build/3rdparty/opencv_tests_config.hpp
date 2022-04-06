54fdc0e70213ad303114b63ad2b8581f005e7586
qw <88542243+qw88888@users.noreply.github.com>
2022-04-06 15:53:20 +0800

    Add files via upload

---
+++ opencv/build/3rdparty/opencv_tests_config.hpp
@@ -0,0 +1,4 @@
+
+#define OPENCV_INSTALL_PREFIX "/home/qw/opencv-3.4.15/install"
+
+#define OPENCV_TEST_DATA_INSTALL_PATH "share/OpenCV/testdata"
---
+++ opencv/build/3rdparty/python_loader/cv2/config.py
@@ -0,0 +1,5 @@
+import os
+
+BINARIES_PATHS = [
+    '/home/qw/opencv-3.4.15/build/lib'
+] + BINARIES_PATHS
