542e1bd4212c5950e1c4e76ed1932316e39f6900
qw <88542243+qw88888@users.noreply.github.com>
2022-04-06 16:20:00 +0800

    Add files via upload

---
+++ opencv/build/opencv_data_config.hpp
@@ -0,0 +1,11 @@
+
+#define OPENCV_INSTALL_PREFIX "/home/qw/opencv-3.4.15/install"
+
+#define OPENCV_DATA_INSTALL_PATH "share/OpenCV"
+
+#define OPENCV_BUILD_DIR "/home/qw/opencv-3.4.15/build"
+
+#define OPENCV_DATA_BUILD_DIR_SEARCH_PATHS \
+    "..//"
+
+#define OPENCV_INSTALL_DATA_DIR_RELATIVE "../share/OpenCV"
